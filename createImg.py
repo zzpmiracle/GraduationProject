56b378de9bb02e8e99184a2b6dc3a302f6c2fa32
zzp <zzpmiracle@gmail.com>
2019-05-17 09:09:10 +0800

    plot

--- createImg.py
+++ createImg.py
@@ -52,10 +52,5 @@ while l < len_data-l_length:
         else:
             pass
     l = l + l_length
-    # begin_time_min = int(np.floor(l / Fs / 60))
-    # begin_time_sec = int(l / Fs % 60)
-    # end_time_min = int(np.floor((l + l_length) / Fs / 60))
-    # end_time_sec = int((l + l_length) / Fs % 60)
-
 
 
