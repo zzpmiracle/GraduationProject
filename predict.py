7a3f6b7057884b0a3665ed96ebccef1a3af1bc2c
zzp <zzpmiracle@gmail.com>
2019-05-20 09:25:41 +0800

    delete useless files

--- predict.py
+++ predict.py
@@ -1,43 +1,59 @@
-
+# -*- coding: utf-8 -*-
+import cv2
+from time import time
 import numpy as np
+import matplotlib.pyplot as plt
+import soundfile as sf
+from keras.preprocessing import image
 from keras.models import load_model
-import os
-from keras.utils import plot_model
-from matplotlib import pyplot as plt
-import math
-from keras.preprocessing.image import ImageDataGenerator
-DenseNet_file_path = './trained_models/denseNet96.2.hdf5'
-test_image_path = '.\wav'
-nb_train_samples = 4000
-nb_val_samples = 500
-nb_test_samples = 151
+# 获取音频数据
+data, samplerate = sf.read('./cr_20181025-161858.wav')
+data = data.reshape((-1, 2))
+data = data.mean(axis=1)
+len_data = len(data)
 
-img_width, img_height = 32,32
-image_dim = (img_width,img_height, 3)
-name_list=list(name[:-4] for name in os.listdir('.\wav\imgs'))
-name_list.sort(key=lambda x:int(x))
-print(name_list)
-batch_size = 64
-test_data_generator = ImageDataGenerator(rescale=1./255).\
-    flow_from_directory(directory=test_image_path,
-                        target_size=(img_width,img_height),
-                        class_mode='binary',
-                        batch_size=batch_size,
-                        shuffle=False)
+l = 0
+Fs = 2000
+
+# DenseNet_file_path = './trained_models/denseNet96.2.hdf5'
+DenseNet_file_path = './trained_models/denseNet-last-96.6.hdf5'
+
+img_width, img_height = 32, 32
 DenseNet_model = load_model(DenseNet_file_path)
-DenseNet_model.compile(loss='binary_crossentropy',
-                       optimizer='Adadelta',
-                       metrics=['accuracy'])
-result = DenseNet_model.predict_generator(test_data_generator,steps=math.ceil(nb_test_samples/batch_size))
+temp_file = 'temp.jpg'
+result_path = 'result.txt'
+#15s
+l_length = 15*Fs
+begin_time_min ,begin_time_sec = 0 , 0
+conti_flag = False
 
-
-for i in range(len(result)):
-    if result[i] < 0.5:
-        l = int(name_list[i])
-        begin_time_min = int(np.floor(l / 2000 / 60))
-        begin_time_sec = int(l / 2000 % 60)
-        end_time_min = int(np.floor((l + 30000) / 2000 / 60))
-        end_time_sec = int((l + 30000) / 2000 % 60)
-        print('{}:{} to {}:{} happened an event!'.format(begin_time_min, begin_time_sec, end_time_min, end_time_sec))
-result = result[:] < 0.5
-print(result)
+with open(result_path,'a') as f:
+    while l < len_data-l_length:
+        batch_data = data[l:l+l_length]
+        p, q, j, k = plt.specgram(batch_data, NFFT=512, Fs=Fs, noverlap=380, cmap='jet')  # 绘制时谱图
+        p = 10 * np.log10(p)
+        p = p[:224][::-1]
+        # plt.imsave('img/{}_{}to{}_{}.png'.format(begin_time_min, begin_time_sec, end_time_min, end_time_sec), p, cmap='jet')
+        plt.imsave(temp_file, p, cmap='jet')
+        img = image.load_img(temp_file, target_size=(img_width, img_height))
+        x = image.img_to_array(img)
+        x *= 1. / 255
+        x = x.reshape([-1, 32, 32, 3])
+        result = DenseNet_model.predict(x)
+        if result<0.5:
+            if conti_flag:
+                    pass
+            else:
+                begin_time_min = int(np.floor(l / Fs / 60))
+                begin_time_sec = int(l / Fs % 60)
+                conti_flag = True
+        else:
+            if conti_flag:
+                end_time_min = int(np.floor((l) / Fs / 60))
+                end_time_sec = int((l) / Fs % 60)
+                conti_flag = False
+                f.writelines('{}:{} to {}:{} happened an event!\n'.format(begin_time_min, begin_time_sec, end_time_min, end_time_sec))
+                # print('{}:{} to {}:{} happened an event!'.format(begin_time_min, begin_time_sec, end_time_min, end_time_sec))
+            else:
+                pass
+        l = l + l_length
--- wav/imgs/imgtest.py
+++
@@ -1,5 +0,0 @@
-from keras.preprocessing import image
-img = image.load_img('0.png',target_size=(32,32))
-img = image.img_to_array(img)
-img *= (1./255)
-print(img)
